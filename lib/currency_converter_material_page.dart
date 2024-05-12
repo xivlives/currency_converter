e5e766e8118ffd148ab1a9aa2b2d66d717a0c73c
Favour Orji <xixlives7@gmail.com>
2024-05-12 15:06:52 +0100

    "Updated CurrencyConverterMaterialPage: changed background color, added variable border styles, updated TextField properties"
    This commit updates the CurrencyConverterMaterialPage by changing the background color, defining border styles in variables and updating the TextField properties

--- lib/currency_converter_material_page.dart
+++ lib/currency_converter_material_page.dart
@@ -4,14 +4,28 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
   const CurrencyConverterMaterialPage({super.key});
   @override
   Widget build(BuildContext context){
+    // declared my border styles in variables
+    const borderDef = OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black12),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                );
+    const onFocusedBorder = OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black, width: 2),
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                );            
     return const Scaffold(
-      backgroundColor: Colors.blueGrey,
+      backgroundColor: Color.fromARGB(255, 95, 182, 254),
       body: Center(child: 
         Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Text("0",
-              style: TextStyle(fontWeight: FontWeight.w800, fontSize: 45, color: Color.fromRGBO(255, 255, 255, 1)),
+              style: TextStyle(
+                fontWeight:
+                FontWeight.w800, 
+                fontSize: 45, 
+                color: Color.fromRGBO(255, 255, 255, 1),
+                ),
             ),
             TextField(
               style: TextStyle(
@@ -19,8 +33,17 @@ class CurrencyConverterMaterialPage extends StatelessWidget{
                 fontSize: 20
               ),
               decoration: InputDecoration(
-                
+                hintText: 'enter ammount in NGN', 
+                hintStyle: TextStyle(color: Colors.white30,
+                ),
+                prefixIcon: Icon(Icons.monetization_on_rounded),
+                prefixIconColor: Colors.white30,
+                filled: true,
+                fillColor: Colors.grey,
+                border: borderDef,
+                focusedBorder: onFocusedBorder,
               ),
+              keyboardType: TextInputType.number,
             )
 
           ],
